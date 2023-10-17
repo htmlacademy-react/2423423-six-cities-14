4207106c528e117a5a24a54bd6189988744e58ff
Diana <ivdiana14500@gmail.com>
2023-10-17 10:59:32 +0300

    fix: add mock file

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,12 @@
 import Main from './components/pages/Main';
 import { IPlaces } from './interfaces/IPlaces';
+import { placesMock } from './mock/Places';
 
 export default function App() {
-  const places: IPlaces[] = [
-    {
-      id: 1,
-      mark: 'Premium',
-      imgUrl: 'img/apartment-01.jpg',
-      price: 120,
-      isBookmarks: false,
-      rating: 80,
-      name: 'Beautiful &amp; luxurious apartment at great location',
-      type: 'Apartment',
-    },
-    {
-      id: 2,
-      mark: '',
-      imgUrl: 'img/room.jpg',
-      price: 80,
-      isBookmarks: true,
-      rating: 80,
-      name: 'Wood and stone place',
-      type: 'Room',
-    },
-    {
-      id: 3,
-      mark: '',
-      imgUrl: 'img/apartment-02.jpg',
-      price: 132,
-      isBookmarks: false,
-      rating: 80,
-      name: 'Canal View Prinsengracht',
-      type: 'Apartment',
-    },
-    {
-      id: 4,
-      mark: 'Premium',
-      imgUrl: 'img/apartment-03.jpg',
-      price: 180,
-      isBookmarks: false,
-      rating: 100,
-      name: 'Nice, cozy, warm big bed apartment',
-      type: 'Apartment',
-    },
-    {
-      id: 5,
-      mark: '',
-      imgUrl: 'img/room.jpg',
-      price: 80,
-      isBookmarks: true,
-      rating: 80,
-      name: 'Wood and stone place',
-      type: 'Room',
-    },
-  ];
+  const places: IPlaces[] = placesMock;
   return (
     <div>
-      <Main places={places}/>
+      <Main places={places} />
     </div>
   );
 }
