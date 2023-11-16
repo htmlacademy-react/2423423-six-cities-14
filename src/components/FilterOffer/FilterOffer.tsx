04130855b17c867de24302527b0f2861f72cb074
Diana <ivdiana14500@gmail.com>
2023-11-16 10:39:21 +0300

    feat: update component

--- src/components/FilterOffer/FilterOffer.tsx
+++ src/components/FilterOffer/FilterOffer.tsx
@@ -39,18 +39,21 @@ function FilterOffer({ isActiveCity }: ICityProps) {
       </b>
       <form className="places__sorting" action="#" method="get">
         <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type" tabIndex={0}>
+        <span
+          className="places__sorting-type"
+          tabIndex={0}
+          onClick={() => setToggleSort(!toggle)}
+        >
           {activeFilterCategory.title}
-          <svg
-            className="places__sorting-arrow"
-            width="7"
-            height="4"
-            onClick={() => setToggleSort(!toggle)}
-          >
+          <svg className="places__sorting-arrow" width="7" height="4">
             <use xlinkHref="#icon-arrow-select"></use>
           </svg>
         </span>
-        <ul className={`places__options places__options--custom ${toggle && 'places__options--opened'}`}>
+        <ul
+          className={`places__options places__options--custom ${
+            toggle && 'places__options--opened'
+          }`}
+        >
           {settings.map((item) => (
             <li
               key={item.id}
