9ada8d889dad1c185bc29e3128f2b298a05adf9f
Diana <ivdiana14500@gmail.com>
2023-11-19 09:56:37 +0300

    feat: add axios

---
+++ src/utils/api.ts
@@ -0,0 +1,14 @@
+// import axios from 'axios';
+// const URL_API = 'https://14.design.pages.academy/six-cities';
+
+// axios({
+//   method: 'get',
+//   url: URL_API,
+// })
+//   .then((response) => {
+//     console.log(`Status code ${response.status}`);
+//     console.log(`Count posts ${response.data.length}`);
+//   })
+//   .catch((err) => {
+//     console.log(`Error: ${err.message}`);
+//   });
