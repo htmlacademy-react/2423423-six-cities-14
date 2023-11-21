eb53c3449ebfcb751bf6148447adaaf0051ceb4a
Diana <ivdiana14500@gmail.com>
2023-11-21 17:16:13 +0300

    bagfix: fix error app componnets

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,18 @@ import Login from './components/pages/Login';
 import Main from './components/pages/Main';
 import NotFound from './components/pages/NotFound';
 import Offer from './components/pages/Offer';
-import { IPlaces } from './interfaces/IPlaces';
-import { placesMock } from './mock/Places';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import PrivateRoute from './components/pages/PrivateRoute';
 import './styles/styles.css';
 import { AppRoute } from './consts/route';
 
 export default function App() {
-  const places: IPlaces[] = placesMock;
-  const [isAuthorized, setIsAuthorized] = useState(true);
+
+  const isAuthorized = true;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Root} element={<Main places={places} />} />
+        <Route path={AppRoute.Root} element={<Main/>} />
         <Route path={AppRoute.Login} element={<Login />} />
         <Route
           path={AppRoute.Favorites}
